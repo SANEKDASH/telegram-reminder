40c81049b82ef732e43789160298d4990c66148a
SANEKDASH <>
2026-08-06 01:40:57 +0300

    main.go: add notification handler

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ func BaseHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
+func NotificationHandler(w http.ResponseWriter, req *http.Request) {
+
+}
+
 func main() {
 	addr, _ := os.LookupEnv("ADDR")
 
 	http.HandleFunc("/", BaseHandler)
+	http.HandleFunc("/notification", NotificationHandler)
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		fmt.Println("Failed to create HTTP server: ", err)
