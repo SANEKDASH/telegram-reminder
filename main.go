dcff339da6c3abef74dade9df8c2d9fa12210f07
SANEKDASH <>
2026-08-07 01:49:46 +0300

    main.go: add definition of Reminder struct

--- main.go
+++ main.go
@@ -4,23 +4,37 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"log"
 )
+
+type Reminder struct {
+	Msg string `json:"msg"`
+	Time string `json:"time"`
+}
 
 func BaseHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Hello!")
 }
 
-func NotificationHandler(w http.ResponseWriter, req *http.Request) {
-
+func ReminderHandler(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		fmt.Fprintf(w, "Get method.")
+	case http.MethodPost:
+		fmt.Fprintf(w, "Post method.")
+	default:
+		log.Printf("Bad request: %v", req)
+		fmt.Fprintf(w, "Error %d", http.StatusBadRequest)
+	}
 }
 
 func main() {
 	addr, _ := os.LookupEnv("ADDR")
 
 	http.HandleFunc("/", BaseHandler)
-	http.HandleFunc("/notification", NotificationHandler)
+	http.HandleFunc("/reminder", ReminderHandler)
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
-		fmt.Println("Failed to create HTTP server: ", err)
+		log.Println("Failed to create HTTP server: ", err)
 	}
 }
