71573629b34e3289d917122327c735d31646ee30
SANEKDASH <>
2026-08-11 23:42:11 +0300

    main.go add handler methods implementation

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 	"log"
+	"sync"
+	"encoding/json"
 )
 
 type Reminder struct {
@@ -12,19 +14,57 @@ type Reminder struct {
 	Time string `json:"time"`
 }
 
+type Reminders struct {
+	rms []Reminder;
+	mu sync.Mutex;
+}
+
+var reminders Reminders;
+
 func BaseHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Hello!")
+	fmt.Fprint(w, "Hello!")
+}
+
+func ListReminders(w http.ResponseWriter, req *http.Request) {
+	reminders.mu.Lock()
+	defer reminders.mu.Unlock()
+
+	err := json.NewEncoder(w).Encode(reminders.rms);
+	if err != nil {
+		http.Error(w, "Internal error.", http.StatusInternalServerError)
+	}
+}
+
+func AddReminder(w http.ResponseWriter, req *http.Request) {
+	var r Reminder;
+	defer req.Body.Close()
+
+	err := json.NewDecoder(req.Body).Decode(&r)
+	if err != nil {
+		http.Error(w, "Invalid body.", http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("%v reminder: %v", req.Method, r)
+
+	reminders.mu.Lock()
+	defer reminders.mu.Unlock()
+	reminders.rms = append(reminders.rms, r);
+
+
+	log.Printf("cur reminders: %v", reminders.rms);
 }
 
 func ReminderHandler(w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodGet:
-		fmt.Fprintf(w, "Get method.")
+		ListReminders(w, req)
 	case http.MethodPost:
+		AddReminder(w, req)
 		fmt.Fprintf(w, "Post method.")
 	default:
 		log.Printf("Bad request: %v", req)
-		fmt.Fprintf(w, "Error %d", http.StatusBadRequest)
+		http.Error(w, "Invalid method.", http.StatusBadRequest)
 	}
 }
 
@@ -33,6 +73,7 @@ func main() {
 
 	http.HandleFunc("/", BaseHandler)
 	http.HandleFunc("/reminder", ReminderHandler)
+
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Println("Failed to create HTTP server: ", err)
