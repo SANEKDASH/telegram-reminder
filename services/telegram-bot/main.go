9ecd44619b64999b49300fcd9240bac31e58a99e
SANEKDASH <>
2026-08-18 00:45:57 +0300

    telegram-bot: add remind handler

--- services/telegram-bot/main.go
+++ services/telegram-bot/main.go
@@ -10,17 +10,36 @@ import (
 	"github.com/go-telegram/bot/models"
 )
 
-
 func helpHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	b.SendMessage(ctx, &bot.SendMessageParams{
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
 		Text:   "This is a help message.",
 	})
+
+	if err != nil {
+		log.Fatalf("Failed to send help handler message: %v", err)
+		return
+	}
+
+	log.Print("Sent help message.")
+}
+
+func remindHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   "What do you want me to remind You in the future?",
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to send remind handler message: %v", err)
+		return
+	}
+
+	log.Print("Sent remind message.")
 }
 
 func main() {
 	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
-	log.Printf("bot token: %v", botToken)
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
@@ -32,6 +51,7 @@ func main() {
 	b, err := bot.New(botToken, opts...)
 
 	b.RegisterHandler(bot.HandlerTypeMessageText, "/help", bot.MatchTypeExact, helpHandler)
+	b.RegisterHandler(bot.HandlerTypeMessageText, "/remind", bot.MatchTypePrefix, remindHandler)
 
 	if err != nil {
 		log.Fatalf("Failed to start bot: %v", err)
